0e1ab91dbfd8a1a1c8bd5d777c9365d7845eb168
Shannon Eric Peevey <peeveys@unlv.nevada.edu>
2013-12-02 16:22:31 -0800

    - add css class option for styling. - add id so that we can call it from javascript. - add ability to have negative lines - use Modernizr.csstransforms to test for older versions of IE differently than newer versions.

--- jquery.line.js
+++ jquery.line.js
@@ -10,71 +10,88 @@
 
 (function($) {
 
-  var helpers = {
-    createLine: function(x1, y1, x2, y2, options){
-      
-                  // Check if browser is Internet Exploder ;)
-                  var isIE = navigator.userAgent.indexOf("MSIE") > -1;
-                  if (x2 < x1){
-                    var temp = x1;
-                    x1 = x2;
-                    x2 = temp;
-                    temp = y1;
-                    y1 = y2;
-                    y2 = temp;
-                  }
-                  var line = document.createElement("div");
-                  
-                  
-                  // Formula for the distance between two points
-                  // http://www.mathopenref.com/coorddist.html
-                  var length = Math.sqrt((x1-x2)*(x1-x2) + (y1-y2)*(y1-y2));
+    var helpers = {
+        createLine: function(x1, y1, x2, y2, options){
+            
+            // Check if browser is Internet Exploder ;)
+            var isIEPre10 = 0;
+            if (!Modernizr.csstransforms) {
+                isIEPre10 = 1;
+            }
+            //var isIE = navigator.userAgent.indexOf("MSIE") > -1;
+            if (x2 < x1){
+                var temp = x1;
+                x1 = x2;
+                x2 = temp;
+                temp = y1;
+                y1 = y2;
+                y2 = temp;
+            }
+            var line = document.createElement("div");
 
-                  line.style.width = length + "px";
-                  line.style.borderBottom = options.stroke + "px solid";
-                  line.style.borderColor = options.color;
-                  line.style.position = "absolute";
-                  line.style.zIndex = options.zindex;
+            // speeves: adding id to div so that we can manipulate it
+            if(options.id !== undefined){
+                line.id = options.id;                      
+            }
+            // speeves: add class so that we can style via stylesheet
+            if(options.myclass != undefined){
+                line.className = options.myclass;                      
+            }else{
+                line.style.borderBottom = options.stroke + "px solid";
+                line.style.borderColor = options.color;
+            }
 
-                  if(isIE){
-                    line.style.top = (y2 > y1) ? y1 + "px" : y2 + "px";
-                    line.style.left = x1 + "px";
-                    var nCos = (x2-x1)/length;
-                    var nSin = (y2-y1)/length;
-                    line.style.filter = "progid:DXImageTransform.Microsoft.Matrix(sizingMethod='auto expand', M11=" + nCos + ", M12=" + -1*nSin + ", M21=" + nSin + ", M22=" + nCos + ")";
-                  }else{
-                    var angle = Math.atan((y2-y1)/(x2-x1));
-                    line.style.top = y1 + 0.5*length*Math.sin(angle) + "px";
-                    line.style.left = x1 - 0.5*length*(1 - Math.cos(angle)) + "px";
-                    line.style.MozTransform = line.style.WebkitTransform = line.style.OTransform= "rotate(" + angle + "rad)";
-                  }
-                  return line;
+            // Formula for the distance between two points
+            // http://www.mathopenref.com/coorddist.html
+            var length = Math.sqrt((x1-x2)*(x1-x2) + (y1-y2)*(y1-y2));
+
+            // set default global styles (inline)
+            // width = the length of the line
+            // zindex = keep the line above everything else
+            line.style.position = "absolute";
+            line.style.width = length + "px";
+            line.style.zIndex = options.zindex;
+
+            var angle = Math.atan((y2-y1)/(x2-x1));
+            //console.log(angle);
+            if(isIEPre10 == 1){
+                line.style.top = (y2 > y1) ? y1 + "px" : y2 + "px";
+                line.style.left = x1 + "px";
+                var nCos = (x2-x1)/length;
+                var nSin = (y2-y1)/length;
+                line.style.filter = "progid:DXImageTransform.Microsoft.Matrix(sizingMethod='auto expand', M11=" + nCos + ", M12=" + -1*nSin + ", M21=" + nSin + ", M22=" + nCos + ")";
+            }else{
+                // speeves: parseFloat(y1), as JS is treating negative 0.5*length*Math.sin(angle) as a string
+                line.style.top = parseFloat(y1) + 0.5*length*Math.sin(angle) + "px";
+                line.style.left = x1 - 0.5*length*(1 - Math.cos(angle)) + "px";
+                line.style.MozTransform = line.style.WebkitTransform = line.style.OTransform = line.style.msTransform =  "rotate(" + angle + "rad)";
+            }
+            return line;
+        }
+    }
+    
+
+    $.fn.line = function( x1, y1, x2, y2, options, callbacks) {
+        return $(this).each(function(){
+            if($.isFunction(options)){
+                callback = options;
+                options = null;
+            }else{
+                callback = callbacks;
+            }
+            options = $.extend({}, $.fn.line.defaults, options);
+
+            $(this).append(helpers.createLine(x1,y1,x2,y2,options)).promise().done(function(){
+                if($.isFunction(callback)){
+                    callback.call();
                 }
-  }
-  
+            });
 
-  $.fn.line = function( x1, y1, x2, y2, options, callbacks) {
-                return $(this).each(function(){
-                  if($.isFunction(options)){
-                      callback = options;
-                      options = null;
-                  }else{
-                    callback = callbacks;
-                  }
-                  options = $.extend({}, $.fn.line.defaults, options);
-
-                  $(this).append(helpers.createLine(x1,y1,x2,y2,options)).promise().done(function(){
-                    if($.isFunction(callback)){
-                      callback.call();
-                    }
-                  });
-
-                
-              });
-  };
-  $.fn.line.defaults = {  zindex : 10000,
-                          color : '#000000',
-                          stroke: "1",
-                        };
+            
+        });
+    };
+    $.fn.line.defaults = {  zindex : 10000,
+                            color : '#000000',
+                            stroke: "1",
+                         };
 })(jQuery);
-
